462d3a5843fe472e296063a5329284b144a60e8a
Randy Eppinger <randy@eppingerconsulting.com>
2020-10-03 19:42:30 -0400

    Able to add transactions. Finished 87

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import './transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,28 +16,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now()),
-  ];
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
-  void addTransaction() {
-    // transactions.add(
-    //   Transaction(
-    //       id: 't${transactions.length}',
-    //       title: titleInput,
-    //       amount: double.parse(amountInput),
-    //       date: DateTime.now()),
-    // );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,76 +34,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    onPressed: () {},
-                    textColor: Colors.purple,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          UserTransactions(),
         ],
       ),
     );
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function _addTransaction;
+
+  NewTransaction(this._addTransaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              onPressed: () {
+                _addTransaction(
+                    titleController.text, double.parse(amountController.text));
+              },
+              textColor: Colors.purple,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
